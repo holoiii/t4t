5c170e23c52aaef9e639b8c6346466e8c4910baa
Albert Peng <pair+albert.peng@socialchorus.com>
2012-07-08 19:13:35 -0700

    Redo twitter keys

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,7 @@
+Twitter.configure do |config|
+  config.consumer_key = 'Cmdbh4ZQfsPhStS37pFDw'
+  config.consumer_secret = 'XcRWiutD3h7HQjFC0DUsXCC2MYogmh4sidNcLe8Rk'
+  config.oauth_token = '630696708-mHWAOUCgOV2x3QW2wy6XAPhZvqbJTR1fV2Gaq3og'
+  config.oauth_token_secret = 'JpQ6MQ3LHwMp4eNIiOiRtQeLX89jGh3VdoFIRM7av9Y'
+  config.connection_options = {:timeout => 5, :open_timeout => 2}
+end
